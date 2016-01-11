6d23b672cc8599503280f706e306ae6f2f250628
ipetrash <gil9red@gmail.com>
2016-01-11 13:56:08 +0500

    Аналог через page-mod работает

--- index.js
+++ index.js
@@ -11,11 +11,9 @@ Your preliminary review request has been approved.
 
 Please note the following for the next update:
 -1) Your add-on prints debugging information to the Console, which is generally not allowed in production add-ons.
-
-2) Listening to both tabs.on('activate', ...) & tabs.on('ready', ...) can result in the script being run multiple times and needlessly.
-
-3) Please use a 'PageMod' instance rather than manually tracking tab events and attaching content scripts.
-https://developer.mozilla.org/en-US/Add-ons/SDK/High-Level_APIs/page-mod
+-2) Listening to both tabs.on('activate', ...) & tabs.on('ready', ...) can result in the script being run multiple times and needlessly.
+-3) Please use a 'PageMod' instance rather than manually tracking tab events and attaching content scripts.
+-https://developer.mozilla.org/en-US/Add-ons/SDK/High-Level_APIs/page-mod
 
 4) Add-on contains duplicate/hidden/unused/unnecessary files or folders. These complicate the review process and may contain sensitive information about the system you created the XPI on. Such files may also create cause for rejection. Please correct your packaging process so that these files aren't included.
 e.g.:
@@ -39,66 +37,176 @@ Thank you.
 
 
 
-var DEBUG = false;
+var DEBUG = true;
 
-DEBUG && console.log("Start plugin");
+function d(mess) {
+    DEBUG && console.log(mess);
+}
+
+d("Start plugin");
 
 // Импортирование модулей
 var buttons = require('sdk/ui/button/action');
 var tabs = require("sdk/tabs");
-
-if (DEBUG) {
-    DEBUG && console.log("Open debug tab");
-
-    tabs.open('http://torrent.mgn.ru');
-    tabs[0].close();
-}
+var pageMod = require("sdk/page-mod");
 
 // Основная часть url раздач, например http://torrent.mgn.ru/viewtopic.php?t=72938
 var URL_TORRENT = 'http://torrent.mgn.ru/viewtopic.php?t=';
+URL_TORRENT = "http://habrahabr.ru/";
+
+if (DEBUG) {
+    d("Open debug tab");
+
+    tabs.open('http://habrahabr.ru');
+    tabs.open('http://habrahabr.ru/post/273397/');
+
+//    // Open a new tab in a new window and make it active.
+//    tabs.open({
+//        url: "http://www.mysite.com",
+//        inNewWindow: true
+//    });
+
+    tabs[0].close();
+}
 
 // Ссылка на кнопку плагина
 var download_torrent_mgn_ru = null;
 
+d("Add handlers activate and open tabs");
 
-function check_tab(tab) {
-    // Проверяем что вкладка относится к URL_TORRENT
-    if (tab.url.startsWith(URL_TORRENT)) {
-        // Создаем кнопку
-        if (download_torrent_mgn_ru == null) {
-            // Кнопка клика кнопки скачивания раздачи и кнопки "спасибо" сайта http://torrent.mgn.ru
-            download_torrent_mgn_ru = buttons.ActionButton({
-                id: "download_torrent_mgn_ru",
-                label: "Download and thank torrent from torrent.mgn.ru",
-                icon: "./tor_icon.jpg",
 
-                // При клике выполняем скрипт
-                onClick: function () {
-                    tab.attach({
-                        contentScriptFile: "./download_and_thank.js"
-                    });
-                }
-            });
-        }
+function createButton() {
+    d("Start create button");
+    d(download_torrent_mgn_ru == null ? "Button not exist, create button" : "Button exist");
 
-    // Иначе, удаляем кнопку
-    } else {
-        if (download_torrent_mgn_ru != null) {
-            download_torrent_mgn_ru.destroy()
-            download_torrent_mgn_ru = null;
-        }
+    // Создаем кнопку
+    if (download_torrent_mgn_ru == null) {
+        // Кнопка клика кнопки скачивания раздачи и кнопки "спасибо" сайта http://torrent.mgn.ru
+        download_torrent_mgn_ru = buttons.ActionButton({
+            id: "download_torrent_mgn_ru",
+            label: "Download and thank torrent from torrent.mgn.ru",
+            icon: "./tor_icon.jpg",
+
+            // При клике выполняем скрипт
+            onClick: function () {
+                // Для активной вкладки вызываем скрипт
+                tabs.activeTab.attach({
+                    contentScriptFile: "./download_and_thank.js"
+                });
+            }
+        });
     }
+
+    d("Finish create button");
 }
 
-DEBUG && console.log("Add handlers activate and open tabs");
+function deleteButton() {
+    d("Start delete button");
+    d(download_torrent_mgn_ru != null ? "Button exist, delete button" : "Button not exist");
 
-// Проверяем вкладку при переходе на нее
-tabs.on('activate', check_tab);
+    if (download_torrent_mgn_ru != null) {
+        download_torrent_mgn_ru.destroy()
+        download_torrent_mgn_ru = null;
+    }
 
-// Проверяем вкладку при открытии
-tabs.on('open', function(tab){
-    tabs.on('ready', check_tab)
+    d("Finish delete button");
+}
+
+
+pageMod.PageMod({
+    //// Not working! Why? O_o
+    //include: "/*habrahabr.ru/*",
+    // TODO: проверить
+    //include: /http:\/\/torrent\.mgn\.ru\/viewtopic\.php\?t=.+/,
+    include: /.+habrahabr.ru\/post.+/,
+
+    attachTo: ["existing", "top"],
+    contentScriptWhen: "start",
+
+    onAttach: function onAttach(worker) {
+        var tab = worker.tab;
+
+        d('onAttach ' + worker.url + ' (' + tab.title + ')');
+
+//        createButton();
+        tab.on('activate', function() {
+            d('on activate tab ' + tab.url + ' start');
+            createButton();
+            d('on activate tab finish');
+        });
+
+        tab.on('pageshow', function() {
+            d('on pageshow tab ' + tab.url + ' start');
+            createButton();
+            d('on pageshow tab finish');
+        });
+
+        tab.on('deactivate', function() {
+            d('on deactivate tab ' + tab.url + ' start');
+            deleteButton();
+            d('on deactivate tab finish');
+        });
+        tab.on('close', function() {
+            d('on close tab ' + tab.url + ' start');
+            deleteButton();
+            d('on close tab finish');
+        });
+    }
 });
 
+// TODO: плагин работает с одним окном
+//// When open new tab as new window, need check url tab -- remove button plugin
+//// for not URL_TORRENT tabs
+//var windows = require("sdk/windows").browserWindows;
+////windows.on('open', function(window) {
+////    var tab = window.tabs.activeTab;
+////    d('on open new window with tab url ' + tab.url + ' start');
+////
+////    if (tab.url.startsWith(URL_TORRENT) == false) {
+////        d('Tab not valid, remove plugin button');
+////        deleteButton();
+////    }
+////
+////    d('on open new window with tab url ' + tab.url + ' finish');
+////});
+//
+//windows.on('activate', function(window) {
+//    var tab = window.tabs.activeTab;
+//    d('on activate new window with tab url ' + tab.url + ' start');
+//
+//    if (tab.url.startsWith(URL_TORRENT) == false) {
+//        d('Tab not valid, remove plugin button');
+//        deleteButton();
+//    }
+//
+//    d('on activate new window with tab url ' + tab.url + ' finish');
+//});
+//
+////windows.on('deactivate', function(window) {
+////    var tab = window.tabs.activeTab;
+////    d('on deactivate new window with tab url ' + tab.url + ' start');
+////
+////    if (tab.url.startsWith(URL_TORRENT) == false) {
+////        d('Tab not valid, remove plugin button');
+////        deleteButton();
+////    }
+////
+////    d('on deactivate new window with tab url ' + tab.url + ' finish');
+////});
+//
+//
+////// When open new tab, need check url tab -- remove button plugin
+////// for not URL_TORRENT tabs
+////tabs.on('open', function(tab) {
+////    d('on open tab ' + tab.url + ' start');
+////
+////    if (tab.url.startsWith(deleteButton) == false) {
+////        d('Tab not valid, remove plugin button');
+////        deleteButton();
+////    }
+////
+////    d('on open tab ' + tab.url + ' finish');
+////});
 
-DEBUG && console.log("Finish plugin");
+
+d("Finish plugin");
